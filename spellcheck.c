d59a505d272604035c0e1bcfeb07e5777fe31fc8
Spiros Avlonitis <spirosa84@hotmail.com>
2018-08-04 06:04:01 +0300

    working on cur_ltr loop controler

--- spellcheck.c
+++ spellcheck.c
@@ -4,21 +4,32 @@
 void spellcheck(SORTNODE *p)
 {
 	int		match;
-	char	word[MAXWORD];
+	char	cur_ltr, *last_word, word[MAXWORD];
 	FILE 	*fp;
 
 	match = 0;
 
+	cur_ltr = *p->t_node->word;
+
+	fp = Fopen("en-US.dic", "r");
 	for ( ; p != NULL ; p = p->next, match = 0 ) {
-		fp = Fopen("en-US.dic", "r");
-		while (getword(fp, word, MAXWORD) != EOF)
+		if (*p->t_node->word > cur_ltr)
+			cur_ltr = *p->t_node->word;
+
+		while (getword(fp, word, MAXWORD) != EOF) {
+			if (cur_ltr > *word)
+				continue;
+			
 			if (strcmp(p->t_node->word, word) == 0) {
 				match = 1;
 				break;
 			}
+		}
+
 		if (match == 0)
 			printf("%s\n", p->t_node->word);
-		fclose(fp);
+		fseek(fp, 0, SEEK_SET);
 	}
+	fclose(fp);
 
 }
