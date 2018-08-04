c91a2070c5557ab1d4328a410655f039f874c57f
Spiros Avlonitis <spirosa84@hotmail.com>
2018-08-04 04:52:16 +0300

    rudimentary version of the spellcheck function

--- spellcheck.c
+++ spellcheck.c
@@ -1,8 +1,24 @@
 #include "hdr.h"
 
 
-
 void spellcheck(SORTNODE *p)
 {
-	
+	int		match;
+	char	word[MAXWORD];
+	FILE 	*fp;
+
+	match = 0;
+
+	for ( ; p != NULL ; p = p->next, match = 0 ) {
+		fp = Fopen("en-US.dic", "r");
+		while (getword(fp, word, MAXWORD) != EOF)
+			if (strcmp(p->t_node->word, word) == 0) {
+				match = 1;
+				break;
+			}
+		if (match == 0)
+			printf("%s\n", p->t_node->word);
+		fclose(fp);
+	}
+
 }
