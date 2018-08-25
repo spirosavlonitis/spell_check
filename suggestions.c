298f9fa6652f414fc1e134e4e8653c317a45d42a
Spiros Avlonitis <spirosa84@hotmail.com>
2018-08-25 06:15:01 +0300

    better suggestions

--- suggestions.c
+++ suggestions.c
@@ -8,7 +8,7 @@ typedef struct s_node {
 void suggestions(char *pattern, int flag)
 {
 	char 	word[MAXWORD], result[MAXWORD];
-	int 	d, i, j, n, l;
+	int 	d, i, j, k, h, n, l;
 	Suggestion sug;
 	FILE	*fp;
 
@@ -26,12 +26,15 @@ void suggestions(char *pattern, int flag)
 		while (getword(fp, word, MAXWORD) != EOF) {
 			if ( (lower(*word)) < (lower(*pattern)) )
 				continue;
-			if ( (lower(word[strlen(word) - 1])) != (lower(pattern[l-1])) )
+			if ( (lower(word[strlen(word) - 1])) != (lower(pattern[l-1])) )		/* same last letter */
 				continue;
 
-			for (i = 0, j = 0, n = 0; i < l ; ++i)
-				if ( (lower(word[i])) == (lower(pattern[n])) )
-					n++;
+			for (i = 0, n = 0; i < strlen(word) ; ++i)
+				for ( j = 0; j < l ; ++j)
+					for (k = j, h = i; k < l && word[k] == pattern[h] ; ++k, ++h)
+						n++;
+					
+
 			if (n > sug.n) {
 				printf(" %s", word);
 				sug.n = n;
